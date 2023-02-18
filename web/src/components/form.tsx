7c3743ca15a009ff469109abb653b6de3e893306
catmiih <emilyleme.dev@gmail.com>
2023-02-18 20:39:48 -0300

    Finished project

--- web/src/components/form.tsx
+++ web/src/components/form.tsx
@@ -16,7 +16,6 @@ export function Form({ onFileUpload }: { onFileUpload: (filename: string) => voi
             try {
                 setIsSubmitting(true);
                 const response = await api.post("/export", formData);
-                console.log(response.data);
                 setFilename(response.data.toString());
             } catch (error) {
                 console.error(error);
@@ -30,7 +29,6 @@ export function Form({ onFileUpload }: { onFileUpload: (filename: string) => voi
     useEffect(() => {
         if (filename) {
             onFileUpload(filename);
-            alert(filename)
             setShouldRedirect(true);
         }
     }, [filename, onFileUpload]);
@@ -42,10 +40,10 @@ export function Form({ onFileUpload }: { onFileUpload: (filename: string) => voi
 
 
     return (
-        <div className="flex justify-center text-center py-3 shadow-xl rounded-xl bg-white" style={{ width: '30rem' }}>
+        <div className="flex justify-center text-center py-0 shadow-xl rounded-xl bg-white" style={{ width: '30rem' }}>
             <div className="w-full mx-14">
-                <h1 className="font-semibold text-2xl text-zinc-700 m-5">Upload your image</h1>
-                <p className="font-regular text-sm text-zinc-600 mb-10">File should be Jpeg, Png, ...</p>
+                <h1 className="font-semibold text-2xl text-zinc-700 mt-5">Upload your image</h1>
+                <p className="font-regular text-sm text-zinc-600">File should be Jpeg, Png, ...</p>
 
                 <form className="flex items-center justify-center w-full">
                     <label className="flex flex-col items-center justify-center w-full h-56 border-dashed border-2 border-blue-200 rounded-lg bg-slate-50 hover:bg-slate-100">
@@ -66,8 +64,8 @@ export function Form({ onFileUpload }: { onFileUpload: (filename: string) => voi
                 <p className="font-regular text-md text-zinc-400 m-3">Or</p>
 
                 <form className="flex items-center justify-center w-full mb-5">
-                    <label className="pointer bg-blue-500 px-5 py-2 rounded-md text-white">
-                        <p>Choose a file</p>
+                    <label className="pointer bg-blue-500 px-5 items-center rounded-md text-white">
+                        <p className="m-0 py-2">Choose a file</p>
                         <input
                             id="dropzone-file"
                             type="file"
--- web/src/components/success.tsx
+++ web/src/components/success.tsx
@@ -1,53 +1,71 @@
 import { useState, useEffect } from 'react';
+import copy from "copy-to-clipboard"; 
 import { BsFillPatchCheckFill } from 'react-icons/bs';
 import { FiCopy } from 'react-icons/fi';
 import { api } from '../lib/axios';
 
-interface successProps {
-    url: string
+interface SuccessProps {
+  url: string;
 }
 
-export function Success(props : successProps) {
+export function Success(props: SuccessProps) {
+  const [imageURL, setImageURL] = useState('');
+  const filename = window.location.pathname.replace('/exporting/file-', '');
 
-    const [imageURL, setImageURL] = useState('');
-    const filename = window.location.pathname.replace('/exporting/file-', '');
+  useEffect(() => {
+    async function fetchImage() {
+      try {
+        const response = await api.get(`${filename}`, { responseType: 'blob' });
+        setImageURL(URL.createObjectURL(response.data));
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchImage();
+  }, [props.url]);
 
-    useEffect(() => {
-        async function fetchImage() {
-            try {
-                const response = await api.get(`${filename}`, { responseType: 'blob' });
-                setImageURL(URL.createObjectURL(response.data));
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        fetchImage();
-    }, [props.url]);
+  const [copyText, setCopyText] = useState('');
+  
+  const handleCopyText = (e : any) => {
+    setCopyText(e.target.value);
+  } 
+    
+  const copyToClipboard = () => {
+    copy(window.location.href);
+    alert(`You have copied "${window.location.href}"`);
+  }
 
-    return (
-        <div className="flex justify-center text-center py-5 pb-10 shadow-xl rounded-xl bg-white" style={{ width: '30rem' }}>
-            <div className="w-full mx-14">
+  return (
+    <div className="flex justify-center text-center py-2 shadow-xl rounded-xl bg-white" style={{ width: '30rem' }}>
+      <div className="w-full mx-14">
+        <BsFillPatchCheckFill className="text-center text-5xl w-full my-3 mt-2 text-green-600 animate-pulse" />
 
-                <BsFillPatchCheckFill className='text-center text-5xl w-full mt-5 text-green-600 animate-bounce duration-75' />
+        <h1 className="font-semibold text-2xl text-zinc-700 m-3">Uploaded Successfully!</h1>
 
-                <h1 className="font-semibold text-2xl text-zinc-700 m-5">Uploaded Successfully!</h1>
-
-                <div className="flex items-center justify-center w-full">
-                    <label className="flex flex-col items-center justify-center w-full h-56 overflow-hidden rounded-lg bg-slate-50">
-                        <img src={imageURL} className='w-full h-56 flex' alt="" />
-                    </label>
-                </div>
-
-                <br />
-
-                <div className='flex border-2 rounded-xl justify-between border-gray-300 p-1'>
-                    <input type="text" name="" value={window.location.href} disabled id="" className='w-full px-3 text-gray-400' />
-                    <label className="pointer bg-blue-500 px-5 py-2 rounded-md text-white">
-                        <FiCopy/>
-                    </label>
-                </div>
-
-            </div>
+        <div className="flex items-center justify-center h-48 lg:w-full overflow-hidden rounded-lg">
+            <img src={imageURL} className="w-full h-full" alt="" />
         </div>
-    )
+
+        <br />
+
+        <div className="flex border-2 rounded-xl mb-5 justify-between border-gray-300 p-1">
+          <input
+            type="text"
+            name=""
+            value={window.location.href}
+            disabled
+            id="urlInput"
+            className="w-full px-3 text-gray-400"
+            onChange={handleCopyText}
+          />
+          <button 
+            className="bg-blue-500 opacity-40 hover:opacity-100 transition-opacity duration-200 px-3 py-2 rounded-md text-white" 
+            onClick={copyToClipboard}
+          >
+            <FiCopy />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
